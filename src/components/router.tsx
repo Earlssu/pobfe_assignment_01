fc269febcf6fbc11da668db8922f0f1f6f9103b0
minseob shim <shim5505@gmail.com>
2023-07-07 17:24:56 +0900

    [Hotfix]문법 통일

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -5,7 +5,7 @@ interface RouterProps {
   children: React.ReactNode;
 }
 
-const Router = ({ children }: RouterProps) => {
+export default function Router({ children }: RouterProps) {
   const [path, setPath] = useState(location.pathname);
   const routes = React.Children.toArray(
     children
@@ -24,6 +24,4 @@ const Router = ({ children }: RouterProps) => {
   }, []);
 
   return routes.find((route) => route.props.path === path);
-};
-
-export default Router;
+}
